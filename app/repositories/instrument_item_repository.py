ad2d76b98550cec2ddace685992b446b11994df7
Do Duong Tam Dang <jacob15072000@gmail.com>
2024-01-29 16:34:05 +0700

    Update instrument_item_repository.py

--- app/repositories/instrument_item_repository.py
+++ app/repositories/instrument_item_repository.py
@@ -3,9 +3,31 @@ from app.models.models import InstrumentItem, db_session
 
 class InstrumentItemRepository:
     def get_all(self, **kwargs):
-        query = db_session.query(InstrumentItem).join(InstrumentItem.instrument)
-        for key, value in kwargs.items():
-            query = query.filter_by(**{key: value})
+        query = db_session.query(InstrumentItem)
+        if kwargs:
+            if kwargs.get("instrument_id"):
+                query = query.filter_by(instrument_id=kwargs.get("instrument_id"))
+            if kwargs.get("serial_number"):
+                query = query.filter_by(serial_number=kwargs.get("serial_number"))
+            if kwargs.get("year_of_purchase"):
+                query = query.filter_by(year_of_purchase=kwargs.get("year_of_purchase"))
+            if kwargs.get("description"):
+                query = query.filter(
+                    InstrumentItem.description.like(f"%{kwargs.get('description')}%")
+                )
+            if kwargs.get("price"):
+                query = query.filter_by(price=kwargs.get("price"))
+            if kwargs.get("category_id", False) or kwargs.get("manufacturer_id", False):
+                query = query.join(InstrumentItem.instrument)
+                if kwargs.get("category_id"):
+                    query = query.filter_by(
+                        category_id=kwargs.get("category_id"),
+                    )
+                if kwargs.get("manufacturer_id"):
+                    query = query.filter_by(
+                        manufacturer_id=kwargs.get("manufacturer_id")
+                    )
+
         return query.all()
 
     def get_by_id(self, instrument_item_id):
